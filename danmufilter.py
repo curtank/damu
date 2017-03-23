fa836bd0aaece3b5159b47ceeabe31446d7de1cb
curtank <zhangzheyu2@yeah.net>
2017-03-23 15:01:35 +0800

    summdatafinish

--- danmufilter.py
+++ danmufilter.py
@@ -11,21 +11,23 @@ def gotjson(filepath):
         pass
     return jsonformats
     pass
-path ='./156277'
-jsonlist=[]
 
-for dirpath,dirnames,filenames in os.walk(path):
-    for filename in filenames:
-        if 'danmu' not in filename:
-            continue
-            pass
-        print(filename)
-        fullpath=os.path.join(dirpath,filename)
-        jsonlist=gotjson(fullpath)
-        jsonlist.extend(jsonlist)
-    pass
+jsonlist=[]
+def sumdata(roomid):
+    path ='./'+str(roomid)
+    for dirpath,dirnames,filenames in os.walk(path):
+        for filename in filenames:
+            if 'danmu' not in filename:
+                continue
+            print(filename)
+            fullpath=os.path.join(dirpath,filename)
+            jsonlist=gotjson(fullpath)
+            jsonlist.extend(jsonlist)
+    storefile=path+"/summdata"
+    pickle.dump(jsonlist,open(storefile,"wb"))
+
 #print(jsonlist)
 storefile="./156277/summdata"
-pickle.dump(jsonlist,open(storefile,"wb"))
 get=pickle.load(open(storefile,"rb"))
-print(type(get))
+print(str(len(get)))
+print(str(type(get[1])))
