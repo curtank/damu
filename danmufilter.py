e74f658b25f1a7bd0185873560d3dd8b7fe3cd72
curtank <zhangzheyu2@yeah.net>
2017-03-23 11:19:28 +0800

    danmufilter

--- danmufilter.py
+++ danmufilter.py
@@ -1,12 +1,22 @@
 import os
+import json
 path ='./156277'
-
+jsonlist=[]
 for dirpath,dirnames,filenames in os.walk(path):
     for filename in filenames:
+        if 'danmu' not in filename:
+            continue
+            pass
         print(filename)
         fullpath=os.path.join(dirpath,filename)
         file=open(fullpath)
-        print(file.readline())
-        dis=file
-        pass
+        for line in file.readlines():
+            jstr=json.loads(line)
+            #print(jstr)
+            pass
     pass
+print(jsonlist)
+jsonstr='{"s":1,"a":2}'
+print(type(jsonstr))
+j=json.loads(jsonstr)
+print(j)
